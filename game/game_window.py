a713175ea7484656560da49f09848987f933bf4f
hongweifei <13071580229@163.com>
2020-07-30 15:47:00 +0800

    2020,7,30

--- game/game_window.py
+++ game/game_window.py
@@ -3,7 +3,7 @@
 
 
 from tkinter import *
-
+from widget.menubar import *
 
 
 class GameWindow(Tk):
@@ -11,6 +11,17 @@ class GameWindow(Tk):
 
     def __init__(self, screenName = None, baseName = None, className = 'Tk', useTk = 1, sync = 0, use = None):
         super().__init__(screenName, baseName, className, useTk, sync, use);
+
+        self.menubar:FlyMenubar = FlyMenubar(self);
+        self.menubar.add_menu("游戏");
+
+        menu_index = [];
+        for i in range(25):
+            menu_index.append(0);
+            self.menubar.add_submenu(str(i + 1),tuple(menu_index));
+        
+
+        self.mainloop();
         
 
 
--- widget/menubar.py
+++ widget/menubar.py
@@ -32,18 +32,18 @@ class Menubar():
     def add_menu(self,text:str) -> int:
         "返回菜单索引"
 
-        new_menu = Menu(self.menubar)
+        new_menu = Menu(self.menubar,tearoff = 0)
         self.menu.append(new_menu)
-        self.menubar.add_cascade(Label = text,menu = new_menu); # 给菜单加菜单
+        self.menubar.add_cascade(label = text,menu = new_menu); # 给菜单加菜单
         return len(self.menu) - 1
         
 
     @staticmethod
-    def menu_add_submenu(root_menu:Menu,text:str) -> Menu:
+    def add_submenu(root_menu:Menu,text:str) -> Menu:
         "返回添加的菜单"
 
-        submenu = Menu(root_menu)
-        root_menu.add_cascade(Label = text,menu = submenu)
+        submenu = Menu(root_menu,tearoff = 0)
+        root_menu.add_cascade(label = text,menu = submenu)
         return submenu
 
 
@@ -52,13 +52,13 @@ class Menubar():
         "返回添加的菜单"
 
         root:Menu = self[menu_index]
-        submenu = Menu(root)
-        root.add_cascade(Label = text,menu = submenu)
+        submenu = Menu(root,tearoff = 0)
+        root.add_cascade(label = text,menu = submenu)
         return submenu
 
 
-    def menu_add_item(self,menu_index:int,text:str,func:function):
-        self.get_menu(menu_index).add_command(Label=text,command=func) # 给菜单加项
+    def menu_add_item(self,menu_index:int,text:str,func):
+        self.get_menu(menu_index).add_command(label = text,command = func) # 给菜单加项
 
 
     def menu_add_separator(self,menu_index:int):
@@ -73,7 +73,7 @@ class FlyMenubar():
     def __init__(self,window:Tk):
         super().__init__()
 
-        self.menubar = Menu(window)
+        self.menubar = Menu(window,tearoff = 0)
         self.menu = {}
 
         window.config(menu=self.menubar)
@@ -81,7 +81,7 @@ class FlyMenubar():
     
     def __getitem__(self, index) -> Menu:
         "返回一级菜单"
-        return self.menu.keys()[index]
+        return list(self.menu.keys())[index]
 
     def get_menu(self,index) -> Menu:
         "返回一级菜单"
@@ -91,29 +91,48 @@ class FlyMenubar():
     def add_menu(self,text:str) -> int:
         "添加一级菜单,返回菜单索引"
 
-        new_menu = Menu(self.menubar)
+        new_menu = Menu(self.menubar,tearoff = 0)
         self.menu[new_menu] = {}
-        self.menubar.add_cascade(Label = text,menu = new_menu); # 给菜单加菜单
+        self.menubar.add_cascade(label = text,menu = new_menu); # 给菜单加菜单
         return len(self.menu) - 1
         
 
-    def menu_add_submenu(self,text:str,*menu_index) -> int:
-        "返回添加的菜单索引"
+    def add_submenu(self,text:str,menu_index:tuple) -> int:
+        """
+        
+        添加子菜单
+
+        menu_index不算item
+
+        例：
+        +-------+
+        |   A   |
+        |   B  >|       注：有 > 为子菜单
+        |   C  >|
+        +-------+
+        如此，
+        0 是 B，不是 A
+        1 是 C
+
+
+        返回添加的菜单索引
+        
+        """
 
         if len(menu_index) > 0:
             prev_menus:dict = self.menu
             prev_menu:Menu = None
 
             for index in menu_index:
-                prev_menu = prev_menus.keys()[index]; # 通过索引获取其key
+                prev_menu = list(prev_menus.keys())[index]; # 通过索引获取其key
                 prev_menus = prev_menus[prev_menu]; # 通过其key获取其value
                 
                 if prev_menu == None or prev_menus == None:
                     return -1
 
-            submenu = Menu(prev_menu)
+            submenu = Menu(prev_menu,tearoff = 0)
             prev_menus[submenu] = {}
-            prev_menu.add_cascade(Label = text,menu = submenu)
+            prev_menu.add_cascade(label = text,menu = submenu)
 
             return len(prev_menus) - 1
         else:
@@ -125,21 +144,22 @@ class FlyMenubar():
         "添加二级菜单，返回添加的菜单索引"
 
         root:Menu = self[menu_index]
-        submenu = Menu(root)
+        submenu = Menu(root,tearoff = 0)
         self.menu[root][submenu] = {}
-        root.add_cascade(Label = text,menu = submenu)
+        root.add_cascade(label = text,menu = submenu)
         return len(self.menu[root]) - 1
 
 
-    def menu_add_item(self,menu_index:int,text:str,func:function):
+    def menu_add_item(self,menu_index:int,text:str,func):
         "给一级菜单添加项"
 
-        self.get_menu(menu_index).add_command(Label=text,command=func) # 给菜单加项
+        m:Menu = self[menu_index]
+        m.add_command(label = text,command = func) # 给菜单加项
 
 
     def menu_add_separator(self,menu_index:int):
         "给一级菜单添加分割线"
 
-        self.get_menu(menu_index).add_separator()
+        self[menu_index].add_separator()
 
     
